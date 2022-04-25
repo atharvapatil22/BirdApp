bab3cf83c85b630a14229191e4e6793dd92567b9
atharvapatil22 <atharva.22.patil@gmail.com>
2022-04-26 01:19:32 +0530

    Initialized project

--- App.js
+++ App.js
@@ -1,11 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./Screens/Home";
+import Catalogue from "./Screens/Catalogue";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ height: "100%" }}>
+      <NavigationContainer>
+        <BottomTab.Navigator
+          // screenOptions={{
+          //   headerRight: () => (
+          //     <View style={{ paddingHorizontal: "5%" }}>
+          //       <Image
+          //         style={{
+          //           width: 160,
+          //           height: 50,
+          //         }}
+          //         source={require("./assets/favicon.png")}
+          //       />
+          //     </View>
+          //   ),
+          // }}
+          initialRouteName="Home"
+          tabBarOptions={{
+            labelStyle: {
+              fontFamily: "TrebuchetMS",
+              fontWeight: "bold",
+            },
+            activeTintColor: "black",
+            inactiveTintColor: "#9E9E9E",
+          }}
+        >
+          <BottomTab.Screen
+            name="Home"
+            component={Home}
+            // children={(data) => <Home />}
+            options={{
+              tabBarLabel: "HOME",
+              tabBarIcon: ({ focused }) => (
+                <Ionicons
+                  name="home"
+                  size={25}
+                  color={focused ? "blue" : "#9E9E9E"}
+                />
+              ),
+            }}
+          />
+          <BottomTab.Screen
+            name="Catalogue"
+            component={Catalogue}
+            // children={(data) => <Catalogue />}
+            options={{
+              tabBarLabel: "CATALOGUE",
+              tabBarIcon: ({ focused }) => (
+                <Ionicons
+                  name="list"
+                  size={25}
+                  color={focused ? "blue" : "#9E9E9E"}
+                />
+              ),
+            }}
+          />
+        </BottomTab.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
@@ -13,8 +75,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Screens/Catalogue.jsx
@@ -0,0 +1,19 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Catalogue = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        // backgroundColor: "blue",
+      }}
+    >
+      <Text>Catalogue</Text>
+    </View>
+  );
+};
+
+export default Catalogue;
