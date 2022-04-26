4bf557492dd4da5ed6cf2cd067e6b93a2421a6ae
atharvapatil22 <atharva.22.patil@gmail.com>
2022-04-26 17:37:24 +0530

    home ui completed

--- Screens/Home.jsx
+++ Screens/Home.jsx
@@ -1,19 +1,94 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  Image,
+  Platform,
+} from "react-native";
+import { MaterialIcons, Ionicons } from "react-native-vector-icons";
+import React, { useState, useEffect } from "react";
+import * as ImagePicker from "expo-image-picker";
 
 const Home = () => {
+  const [image, setImage] = useState(null);
+
+  const camImg = async () => {};
+
+  const pickImage = async (type) => {
+    let result;
+    const options = {
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      aspect: [3, 4],
+      quality: 1,
+    };
+
+    if (type === "camera") {
+      result = await ImagePicker.launchCameraAsync(options);
+    } else if (type === "gallery") {
+      result = await ImagePicker.launchImageLibraryAsync({
+        ...options,
+        allowsEditing: true,
+      });
+    }
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        // backgroundColor: "blue",
       }}
     >
-      <Text>Home</Text>
+      <Text style={{ fontSize: 25, marginBottom: 20 }}>Select a picture</Text>
+      {/* {image && (
+        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      )} */}
+      <TouchableOpacity
+        onPress={() => pickImage("camera")}
+        style={styles.button}
+      >
+        <Ionicons name={"camera"} color={"white"} size={50} />
+        <Text style={styles.label}>Camera</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => pickImage("gallery")}
+        style={styles.button}
+      >
+        <MaterialIcons name={"collections"} size={50} color={"white"} />
+        <Text style={styles.label}>Gallery</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default Home;
+
+const { height, width } = Dimensions.get("window");
+const heightSc = height / 1000;
+const widthSc = width / 1000;
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "blue",
+    width: widthSc * 600,
+    height: 150,
+    marginVertical: heightSc * 30,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 12,
+  },
+  label: {
+    color: "white",
+    fontSize: 20,
+    marginTop: 10,
+  },
+});
