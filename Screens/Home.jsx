85fdbb02f0b6f4edfe416be43b23a100fe983f61
atharvapatil22 <atharva.22.patil@gmail.com>
2022-05-20 01:51:13 +0530

    added modal

--- Screens/Home.jsx
+++ Screens/Home.jsx
@@ -6,6 +6,9 @@ import {
   Dimensions,
   Image,
   Platform,
+  Alert,
+  Modal,
+  Pressable,
 } from "react-native";
 import { MaterialIcons, Ionicons } from "react-native-vector-icons";
 import React, { useState, useEffect } from "react";
@@ -13,29 +16,53 @@ import * as ImagePicker from "expo-image-picker";
 import axios from "axios";
 import { birdsList } from "../birdData";
 
-const Home = () => {
+const Home = ({ navigation }) => {
   const [image, setImage] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [onViewInfo, setOnViewInfo] = useState(() => {});
+  const [detectedBird, setDetectedBird] = useState("");
 
   const getPrediction = () => {
-    // const myFormData = new FormData();
-    // myFormData.append("file", image);
+    const myFormData = new FormData();
+
+    // const random_num = Math.floor(Math.random() * 10000);
+    // const imageName = random_num.toString() + ".jpg";
+
+    // console.log("image name:", imageName);
+
+    myFormData.append("file", {
+      uri: image,
+      name: "xyhjhhjjgj.jpg",
+      type: `image/jpg`,
+    });
 
     // axios
-    //   .post("https://0793-103-200-75-160.in.ngrok.io/predict", myFormData, {
+    //   .post("https://ca16-103-200-75-174.in.ngrok.io/predict", myFormData, {
     //     headers: {
     //       "Content-Type": "multipart/form-data",
     //     },
     //   })
     //   .then((res) => {
-    //     // put code snippet here
-    //     console.log(res);
-    //   })
-    //   .catch((err) => console.log(err.response.data));
+    //     console.log("pred: ", res.data.prediction);
 
-    // SNIPPET:
-    const prediction = "COPPERSMITH BARBET";
-    const birdObject = birdsList.filter((item) => item.birdName == prediction);
-    console.log(birdObject);
+    //     const prediction = res.data.prediction;
+    //     setDetectedBird(prediction);
+
+    //     // REMOVE CODE ---->
+    //     // const birdObject = birdsList.filter(
+    //     //   (item) => item.birdName == prediction
+    //     // )[0];
+    //     // console.log(birdObject);
+    //     // setOnViewInfo(() => {
+    //     //   navigation.navigate("BirdInfo", birdObject);
+    //     // });
+    //     // REMOVE CODE ----<
+    //     setShowModal(true);
+    //   })
+    //   .catch((err) => console.log("error: ", err?.response?.data));
+
+    setDetectedBird("WHITE RUMPED VULTURE");
+    setShowModal(true);
   };
 
   const pickImage = async (type) => {
@@ -63,6 +90,66 @@ const Home = () => {
     }
   };
 
+  // MODAL
+
+  function MyModal() {
+    const [modalVisible, setModalVisible] = useState(true);
+    return (
+      <View style={styles.centeredView}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={{ fontSize: 16 }}>Detected: {detectedBird}</Text>
+
+              <View
+                style={{
+                  flexDirection: "row",
+                  marginTop: 20,
+                  justifyContent: "space-between",
+                  width: "80%",
+                }}
+              >
+                <Pressable
+                  style={[styles.modalButton, styles.buttonClose]}
+                  // onPress={() => setModalVisible(!modalVisible)}
+                  onPress={() => {
+                    const birdObject = birdsList.filter(
+                      (item) => item.birdName == detectedBird
+                    )[0];
+                    console.log(birdObject);
+                    navigation.navigate("BirdInfo", birdObject);
+
+                    setModalVisible(!modalVisible);
+                    setShowModal(false);
+                  }}
+                >
+                  <Text style={styles.textStyle}>View Info</Text>
+                </Pressable>
+                <Pressable
+                  style={[styles.modalButton, styles.buttonClose]}
+                  onPress={() => {
+                    setModalVisible(!modalVisible);
+                    setShowModal(false);
+                  }}
+                >
+                  <Text style={styles.textStyle}>Go back</Text>
+                </Pressable>
+              </View>
+            </View>
+          </View>
+        </Modal>
+      </View>
+    );
+  }
+
   return (
     <View
       style={{
@@ -71,6 +158,12 @@ const Home = () => {
         alignItems: "center",
       }}
     >
+      {true && (
+        <View style={{ position: "absolute" }}>
+          <MyModal setShowModal={setShowModal} />
+        </View>
+      )}
+
       <Text style={{ fontSize: 25, marginBottom: 20 }}>Select a picture</Text>
       {/* {image && (
         <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
@@ -113,5 +206,57 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 20,
     marginTop: 10,
+  },
+
+  // Model styles
+
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.35)",
+  },
+  modalView: {
+    width: "80%",
+    height: 250,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalButton: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "blue",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
